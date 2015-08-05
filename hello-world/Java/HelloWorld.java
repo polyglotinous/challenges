a2ed5a8fc8ddba7217438b709b36d1acbd3f4054
Chris Davies <ferociousturtle@gmail.com>
2015-08-05 10:56:44 -0400

    Make HelloWorld.java have consistent spacing and indentation

--- hello-world/Java/HelloWorld.java
+++ hello-world/Java/HelloWorld.java
@@ -1,9 +1,8 @@
 // Requires Java: http://www.oracle.com/technetwork/java/javase/downloads/index.html
 // Run in terminal: javac HelloWorld.java (hit enter) java HelloWorld
-public class HelloWorld
-  {
-	  public static void main( String[] args)
-	    {
-		    System.out.println( "Hello World!" );
-	    }
-  }
+
+public class HelloWorld {
+    public static void main( String[] args) {
+        System.out.println( "Hello World!" );
+    }
+}
