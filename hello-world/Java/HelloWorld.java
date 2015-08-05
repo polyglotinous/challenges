a308d7e666f75bb962c4a44f3516302fc6b49a14
Skipper Davies <skipperdavies@hotmail.com>
2015-08-05 10:02:24 -0400

    Update HelloWorld.java

--- hello-world/Java/HelloWorld.java
+++ hello-world/Java/HelloWorld.java
@@ -1,3 +1,5 @@
+// Requires Java: http://www.oracle.com/technetwork/java/javase/downloads/index.html
+// Run in terminal: javac HelloWorld.java (hit enter) java HelloWorld
 public class HelloWorld
   {
 	  public static void main( String[] args)
