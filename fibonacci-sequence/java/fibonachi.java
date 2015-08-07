4416ef480732f5e6e1efd5448ac4f8a195810528
Skipper Davies <skipperdavies@hotmail.com>
2015-08-07 16:02:53 -0400

    Update fibonachi.java

--- fibonacci-sequence/java/fibonachi.java
+++ fibonacci-sequence/java/fibonachi.java
@@ -1,6 +1,5 @@
 public class fibonachi{
-    public static void main(String[] args) {
-
+    public static void main(String[] args){
      int prevVal = 1;
      int nextVal = 0;
      int count = 0;
@@ -10,8 +9,7 @@ public class fibonachi{
          prevVal = start;
          System.out.print(start+" ");
          count++;
-     }
-     
-         while(count<11);
+        }
+        while(count<11);
     }    
 }
