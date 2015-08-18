677d5db6c5b123403ea5bd02ff5f38eaa1e68338
Skipper Davies <skipperdavies@hotmail.com>
2015-08-18 13:39:37 -0400

    Create replacing.java

---
+++ first-amendment/java/replacing.java
@@ -0,0 +1,9 @@
+public class replacing{
+    public static void main(String[] args){
+        String aString = "Where is Teve Torbes?";
+        
+        String amend = aString.replace("Teve Torbes", "Steve Forbes");
+        
+        System.out.println(amend);
+    } 
+}
